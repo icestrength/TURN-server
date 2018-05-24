e7a1be330441de8ad53bdf44dfe756af90a873da
Alex <hrytsyna1@gmail.com>
2018-05-24 11:29:58 +0300

    Edited App.jsx

--- js/App.jsx
+++ js/App.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const FourOhFour = () => <h1>404</h1>;
+const FourOhFour = () => <h1>404402</h1>;
 
 const App = () => (
   <BrowserRouter>
